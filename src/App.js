b5a687583c448292fedc1585b991f738fe0dcd80
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2020-11-05 22:50:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React,{ Fragment, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MyCard from "./components/MyCard";
+import { getMatches } from "./api/api";
+import { Container, Grid, Typography } from "@material-ui/core";
+function App() {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    getMatches()
+      .then((data) => {
+        console.log(data.matches);
+        setMatches(data.matches);
+      })
+      .catch((error) => {});
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Typography variant="h3" style={{marginTop:20}}>
+      Welcome to Cricket App
+      </Typography>
+
+      <Container>
+        <Grid container>
+          <Grid sm="2"></Grid>
+          <Grid sm="8">
+          {
+            matches &&
+            <ul>
+            {
+              matches.map(match => <li key={match.id}>{match.date}</li>)
+            }
+            </ul>
+            }
+
+              { matches && matches.map((match) => {
+                return(
+                <Fragment>
+              { match.type=="Twenty20" ? (<MyCard key={match.unique_id} match={match} />)
+              : (""
+                )
+              }
+
+               </Fragment>);
+            })
+          }
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
